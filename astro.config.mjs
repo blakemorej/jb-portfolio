04a07a3788407d727943903e199652bf9e6fb75a
blakemorej <blakemorej93@gmail.com>
2026-05-12 21:36:13 +0100

    feat: update domain to jonblakemore.com

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://jblakemore-seo.com',
+  site: 'https://jonblakemore.com',
   vite: {
     plugins: [tailwindcss()]
   }
